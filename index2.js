4c6b2f1287305ebbf3a67ec2b539ff618c597b2c
vitalyvitmens <otm_@tut.by>
2022-12-02 20:02:10 +0300

    added data in project JavaScript

--- index2.js
+++ index2.js
@@ -3,31 +3,69 @@ import fs from "fs";
 import path from "path";
 
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    fs.readFile(path.join("public", "index.html"), (err, data) => {
-      if (err) {
-        throw err;
-      }
+  // if (req.url == "/") {
+  //   fs.readFile(path.join("public", "index.html"), (err, data) => {
+  //     if (err) {
+  //       throw err;
+  //     }
 
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-      });
-      res.end(data);
-    });
-  } else if (req.url === "/contact") {
-    fs.readFile(path.join("public", "contact.html"), (err, data) => {
-      if (err) {
-        throw err;
-      }
+  //     res.writeHead(200, {
+  //       "Content-Type": "text/html",
+  //     });
+  //     res.end(data);
+  //   });
+  // } else if (req.url === "/contact") {
+  //   fs.readFile(path.join("public", "contact.html"), (err, data) => {
+  //     if (err) {
+  //       throw err;
+  //     }
 
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-      });
-      res.end(data);
-    });
+  //     res.writeHead(200, {
+  //       "Content-Type": "text/html",
+  //     });
+  //     res.end(data);
+  //   });
+  // }
+
+  let filePath = path.join("public", req.url === "/" ? "index.html" : req.url);
+  const ext = path.extname(filePath);
+  let contentType = "text/html";
+
+  switch (ext) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    default:
+      contentType = "text/html";
   }
+
+  if (!ext) {
+    filePath += ".html";
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      fs.readFile(path.join("public", "error.html"), (err, data) => {
+        if (err) {
+          res.writeHead(500);
+          res.end("Error");
+        } else {
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        }
+      });
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content);
+    }
+  });
 });
 
-server.listen(3000, () => {
-  console.log("Server has been started...");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}...`);
 });

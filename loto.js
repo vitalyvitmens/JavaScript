09ac27cc490eb8433446cfa0fa9a6d93ef3589e8
vitalyvitmens <otm_@tut.by>
2024-07-07 21:48:12 +0300

    feat: add fn containsResult

--- loto.js
+++ loto.js
@@ -1,4 +1,4 @@
-import { results } from "./result.js"
+import { results } from './result.js'
 
 // Функция для подсчета частоты каждого числа
 function countFrequency(numbers) {
@@ -27,3 +27,20 @@ const frequency = countFrequency(results)
 const hotNumbers = getMostFrequentNumbers(frequency)
 
 console.log('Наиболее вероятные числа для следующего розыгрыша:', hotNumbers)
+
+// Функция для проверки, содержится ли результат в массиве
+function containsResult(draws, result) {
+  // Сортируем результат для последующего сравнения
+  const sortedResult = result.slice().sort((a, b) => a - b)
+
+  // Проверяем каждый розыгрыш на соответствие результату
+  return draws.some((draw) => {
+    const sortedDraw = draw.slice().sort((a, b) => a - b)
+    return sortedDraw.every((num, index) => num === sortedResult[index])
+  })
+}
+
+const newResult = [10, 25, 31, 32, 42, 49]
+const isPresent = containsResult(results, newResult)
+
+console.log(`Встречалась ли ставка ${newResult} ранее в розыгрыше:`, isPresent) // Выведет true или falseы
